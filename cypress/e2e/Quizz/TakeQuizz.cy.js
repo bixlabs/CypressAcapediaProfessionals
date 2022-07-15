10f029873539dc96215478fdf86b27fdb47c3332
lguedesbix <101207116+lguedesbix@users.noreply.github.com>
2022-07-15 16:28:00 -0300

    Update TakeQuizz.cy.js

--- cypress/e2e/Quizz/TakeQuizz.cy.js
+++ cypress/e2e/Quizz/TakeQuizz.cy.js
@@ -3,43 +3,35 @@ describe('TakeQuiz', () => {
     cy.fixture('/auth/credentialsLogin')
       .as('credentials')
       .then(cy.loginAccount)
-  });
+  })
 
-  it('Can take a quiz', function() {
-      cy.get('[aria-selected="false"]').click()
-      cy.get(':nth-child(1) > .v-card__actions > .primary--text > .v-btn__content').as('getCME')
-      cy.get('@getCME').click({ force: true })
-  
-      cy.getByTestId('questionChipList').each( ($el, index, $list) => {
-  
-        cy.wrap($el).click({ force: true }).then( () => {
+  it('Can take a quiz', function () {
+    cy.get('[aria-selected="false"]').click()
+    cy.get(':nth-child(1) > .v-card__actions > .primary--text > .v-btn__content').as('getCME')
+    cy.get('@getCME').click({ force: true })
 
-          cy.intercept({
-            method: 'POST',
-            url: '/api/article/quiz/question/answer',
-          }).as('answer')
-        
-          cy.getByTestId('answerOption').its('length').then( (optionsLength) => {
-          cy.getByTestId('answerOption').eq(Math.floor(Math.random()*optionsLength)).click({ force: true })
-          })
-  
-          cy.wait('@answer')
-            .its('response.statusCode')
-            .should('equal', 200)
-            .then( () => {
-              cy.getByTestId('feedbackAnswer').then( $element => {
-                let feedback = $element.text().trim()
-                if (feedback === "It's correct") {
-                  cy.getByTestId('quizPanel').click('topLeft', {force: true})
-                } else {
-                  cy.getByTestId('nextQuestion').click({force: true})
-                }
-              })
+    cy.getByTestId('questionChipList').each(($el, index, $list) => {
+      cy.wrap($el).then(() => {
+
+        cy.intercept({
+          method: 'POST',
+          url: '/api/article/quiz/question/answer',
+        }).as('answer')
+
+        cy.getByTestId('answerOption').its('length').then((optionsLength) => {
+          cy.getByTestId('answerOption').eq(Math.floor(Math.random() * optionsLength)).click({ force: true })
+          cy.contains('Confirm answer').click()
+        })
+
+        cy.wait('@answer')
+          .its('response.statusCode')
+          .should('equal', 200)
+          .then( () => {
+            cy.get('.col-sm-4 > .row > .text-right > .mt-sm-2').click()
             })
-          
         })
       })
-  
+
   })
 })
- 
+
