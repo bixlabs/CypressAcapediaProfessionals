579351b086f233c4d23ba9ac342f41707dcb1356
Toni Cabrera <acabreragnz@gmail.com>
2023-03-16 21:27:34 -0300

    fix: add /api prefix to login calls and auto format code style

--- cypress/e2e/Acapedia/Login.cy.js
+++ cypress/e2e/Acapedia/Login.cy.js
@@ -1,27 +1,23 @@
 describe('LogIn E2E Test', () => {
-    before(function () {
-        cy.fixture('auth/credentialsLogin').as('credentials')
-    })
+  before(function () {
+    cy.fixture('auth/credentialsLogin').as('credentials');
+  });
 
-    it('LogIn', function() {
-        cy.visit('/login')
-        cy.getByTestId('email').type(this.credentials.email)
-        cy.getByTestId('password').type(this.credentials.password)
+  it('LogIn', function () {
+    cy.visit('/login');
+    cy.getByTestId('email').type(this.credentials.email);
+    cy.getByTestId('password').type(this.credentials.password);
 
-        cy.intercept({
-            method: 'POST',
-            url: '/login',
-        }).as('login')
+    cy.intercept({
+      method: 'POST',
+      url: '/api/login',
+    }).as('login');
 
-        cy.contains('Log in').click()
+    cy.contains('Log in').click();
 
-        cy.wait('@login')
-        .its('response.statusCode')
-        .should('equal', 200)
+    cy.wait('@login').its('response.statusCode').should('equal', 200);
 
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('.text-decoration-none > .d-flex').as('.credit-box').should('exist')
-
-    })
-
-})
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('.text-decoration-none > .d-flex').as('.credit-box').should('exist');
+  });
+});
--- cypress/support/commands/auth_commands.js
+++ cypress/support/commands/auth_commands.js
@@ -1,63 +1,60 @@
-Cypress.Commands.add('registerAccount', ({ 
-    email = 'lguedes+600@bixlabs.com', 
-    password = 'Ab1234567-', 
-    firstName = 'Leonardo', 
-    lastName = 'Guedes', 
-    phoneNumber = '5612023378'
-    } = {}) => {
+Cypress.Commands.add(
+  'registerAccount',
+  ({
+    email = 'lguedes+600@bixlabs.com',
+    password = 'Ab1234567-',
+    firstName = 'Leonardo',
+    lastName = 'Guedes',
+    phoneNumber = '5612023378',
+  } = {}) => {
+    cy.visit('/register');
+    cy.getByTestId('email').type(email);
+    cy.getByTestId('password').type(password);
 
-        cy.visit('/register')
-        cy.getByTestId('email').type(email)
-        cy.getByTestId('password').type(password)
-
-        cy.contains('Sign up').click()
-
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('[name=firstName]').type(firstName)
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('[name=lastName]').type(lastName)
-        cy.contains('Continue').click()
-
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('#input-66').as('phone')
-        cy.get('@phone').type(phoneNumber)
-        cy.contains('Send SMS').click()
-
-        cy.intercept({
-            method: 'POST',
-            url: '/api/register',
-        }).as('register')
-
-        cy.getByTestId('phoneCode').first().type('375736')
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get(':nth-child(1) > .actions > :nth-child(1) > .heading').as('continue')
-        cy.get('@continue').click()
-
-        cy.wait('@register')
-        .its('response.statusCode')
-        .should('equal', 200)
-
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('.onboarding-panel').should('exist')
-
-})
-
-Cypress.Commands.add('loginAccount', () => {
-    cy.visit('/login')
-    cy.getByTestId('email').type("lguedes+031@bixlabs.com")
-    cy.getByTestId('password').type("Ab1234567-")
-    
-    cy.intercept({
-        method: 'POST',
-        url: '/login',
-    }).as('login')
-
-    cy.contains('Log in').click()
-
-    cy.wait('@login')
-    .its('response.statusCode')
-    .should('equal', 200)
+    cy.contains('Sign up').click();
 
     // TODO: we need a test-id here as we cannot get it by text value
-    cy.get('.text-decoration-none > .d-flex').as('.credit-box').should('exist')
-})
+    cy.get('[name=firstName]').type(firstName);
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('[name=lastName]').type(lastName);
+    cy.contains('Continue').click();
+
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('#input-66').as('phone');
+    cy.get('@phone').type(phoneNumber);
+    cy.contains('Send SMS').click();
+
+    cy.intercept({
+      method: 'POST',
+      url: '/api/register',
+    }).as('register');
+
+    cy.getByTestId('phoneCode').first().type('375736');
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get(':nth-child(1) > .actions > :nth-child(1) > .heading').as('continue');
+    cy.get('@continue').click();
+
+    cy.wait('@register').its('response.statusCode').should('equal', 200);
+
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('.onboarding-panel').should('exist');
+  },
+);
+
+Cypress.Commands.add('loginAccount', () => {
+  cy.visit('/login');
+  cy.getByTestId('email').type('lguedes+031@bixlabs.com');
+  cy.getByTestId('password').type('Ab1234567-');
+
+  cy.intercept({
+    method: 'POST',
+    url: '/api/login',
+  }).as('login');
+
+  cy.contains('Log in').click();
+
+  cy.wait('@login').its('response.statusCode').should('equal', 200);
+
+  // TODO: we need a test-id here as we cannot get it by text value
+  cy.get('.text-decoration-none > .d-flex').as('.credit-box').should('exist');
+});
