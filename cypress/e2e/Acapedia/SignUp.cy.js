fa957ba0988ec833414205df161e186ed0138bc7
Toni Cabrera <acabreragnz@gmail.com>
2023-03-22 18:54:05 -0300

    fix: signup test using an id autogenerated as selector

--- cypress/e2e/Acapedia/SignUp.cy.js
+++ cypress/e2e/Acapedia/SignUp.cy.js
@@ -1,63 +1,62 @@
 describe('SignUp E2E Test', () => {
-    before(function () {
-        cy.visit('/register')
+  before(function () {
+    cy.visit('/register');
 
-        cy.fixture('auth/credentialsSignup').as('credentials')
-    })
+    cy.fixture('auth/credentialsSignup').as('credentials');
+  });
 
-    it('SignUp', function() {
-        cy.getByTestId('email').type(this.credentials.email)
-        cy.getByTestId('password').type(this.credentials.password)
+  it('SignUp', function () {
+    cy.getByTestId('email').type(this.credentials.email);
+    cy.getByTestId('password').type(this.credentials.password);
 
-        cy.contains('Sign up').click()
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('[name=firstName]').type(this.credentials.firstName)
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('[name=lastName]').type(this.credentials.lastName)
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get(('.v-select__selections')).as('degree').click()
-        cy.contains('M.D').click()
-        cy.contains('Continue').click()
+    cy.contains('Sign up').click();
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('[name=firstName]').type(this.credentials.firstName);
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('[name=lastName]').type(this.credentials.lastName);
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('.v-select__selections').as('degree').click();
+    cy.contains('M.D').click();
+    cy.contains('Continue').click();
 
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('.text-left > .v-text-field--single-line > .v-input__control > .v-input__slot > .v-select__slot > .v-select__selections').as('selectMedicalBoard').click()
-        cy.contains('American Board of Anesthesia').click()
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('#boardId').type('123456')
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('#input-90').as('dateBirth').click()
-        cy.contains('28').click()
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('.v-window-item--active > .mt-5 > :nth-child(1) > :nth-child(1) > :nth-child(2) > .container-actions > :nth-child(1) > .heading').as('Continue').click()
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get(
+      '.text-left > .v-text-field--single-line > .v-input__control > .v-input__slot > .v-select__slot > .v-select__selections',
+    )
+      .as('selectMedicalBoard')
+      .click();
+    cy.contains('American Board of Anesthesia').click();
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('#boardId').type('123456');
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.getByTestId('boardDateOfBirdInput').click();
+    cy.contains('28').click();
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get(
+      '.v-window-item--active > .mt-5 > :nth-child(1) > :nth-child(1) > :nth-child(2) > .container-actions > :nth-child(1) > .heading',
+    )
+      .as('Continue')
+      .click();
 
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('#input-122').as('phone')
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('@phone').type(this.credentials.phoneNumber)
-        cy.contains('Send SMS').click()
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('#input-122').as('phone');
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('@phone').type(this.credentials.phoneNumber);
+    cy.contains('Send SMS').click();
 
-        cy.intercept({
-            method: 'POST',
-            url: '/api/register',
-        }).as('register')
+    cy.intercept({
+      method: 'POST',
+      url: '/api/register',
+    }).as('register');
 
-        cy.getByTestId('phoneCode').first().type('720021')
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get(':nth-child(3) > .row > .container-actions > :nth-child(1) > .heading').as('continue')
-        cy.get('@continue').click()
+    cy.getByTestId('phoneCode').first().type('720021');
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get(':nth-child(3) > .row > .container-actions > :nth-child(1) > .heading').as('continue');
+    cy.get('@continue').click();
 
-        cy.wait('@register')
-        .its('response.statusCode')
-        .should('equal', 200)
+    cy.wait('@register').its('response.statusCode').should('equal', 200);
 
-        // TODO: we need a test-id here as we cannot get it by text value
-        cy.get('.onboarding-panel').should('exist')
-
-    })
-
-})
-
-
-
-
-
+    // TODO: we need a test-id here as we cannot get it by text value
+    cy.get('.onboarding-panel').should('exist');
+  });
+});
