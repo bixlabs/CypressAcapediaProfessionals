7f24874914d8ee1b30829546ee6d84f63b8da74c
Toni Cabrera <acabreragnz@gmail.com>
2023-08-11 00:33:33 -0300

    add more tests for regression bug of pretest

---
+++ cypress/e2e/Pretest/RegressionBugs/865cwxz4q.cy.js
@@ -0,0 +1,117 @@
+// https://app.clickup.com/t/865cwxz4q
+
+describe('Regression Bug: Pre-test "End" screen without having done the pre-test yet', () => {
+  beforeEach(() => {
+    cy.fixture('/auth/credentialsLogin').as('credentials');
+    cy.loginAccount('@credentials');
+  });
+
+  it('should not show the "End" screen after completing a pre-test and starting a new one', () => {
+    // Intercept the request to always show the pretest
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/user/pretest/show',
+      },
+      {
+        showPretest: true,
+      },
+    ).as('showPretest');
+
+    const articles = ['Production of single cell', 'Effectiveness of COVID-19'];
+
+    cy.get('[name="search"]:visible').type(`${articles[0]}{enter}`);
+    cy.contains('Get CME').click();
+    cy.contains('Next').click();
+    cy.contains('Start pre-test').click();
+
+    const firstOptionSelector = '.v-input--radio-group__input > :nth-child(1)';
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.contains('Read article and take quiz').click();
+    cy.contains('Back to Feed').click();
+    cy.get('[name="search"]:visible').type(`${articles[1]}{enter}`);
+    cy.contains('Get CME').click();
+    cy.get('.align-center > .heading').should('have.text', ' What is a pre-test? ');
+  });
+
+  it('should now show the initial step of the pretest onboarding, after completing the second pretest', () => {
+    // Intercept the request to always show the pretest
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/user/pretest/show',
+      },
+      {
+        showPretest: true,
+      },
+    ).as('showPretest');
+
+    const articles = ['Production of single cell', 'Effectiveness of COVID-19'];
+
+    cy.get('[name="search"]:visible:visible').type(`${articles[0]}{enter}`);
+    cy.contains('Get CME').click();
+    cy.contains('Next').click();
+    cy.contains('Start pre-test').click();
+
+    const firstOptionSelector = '.v-input--radio-group__input > :nth-child(1)';
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.contains('Read article and take quiz').click();
+    cy.contains('Back to Feed').click();
+
+    cy.get('[name="search"]:visible').type(`${articles[1]}{enter}`);
+    cy.contains('Get CME').click();
+    cy.contains('Next').click();
+    cy.contains('Start pre-test').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+
+    cy.get('.align-center > .heading').should('have.text', ' Thank you! ');
+    cy.get('.descriptions').should(
+      'have.text',
+      'Now you can read the article, complete the post-test and get CME credit.',
+    );
+  });
+
+  it('should show the overlay when returning to the results step of the pretest', () => {
+    // Intercept the request to always show the pretest
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/user/pretest/show',
+      },
+      {
+        showPretest: true,
+      },
+    ).as('showPretest');
+
+    cy.get('[name="search"]').type('Production of single cell{enter}');
+    cy.contains('Get CME').click();
+    cy.contains('Next').click();
+    cy.contains('Start pre-test').click();
+
+    const firstOptionSelector = '.v-input--radio-group__input > :nth-child(1)';
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+    cy.get(firstOptionSelector).click();
+    cy.contains('Confirm answer').click();
+
+    cy.get('.align-center > .heading').should('have.text', ' Thank you! ');
+    cy.intercept('/api/feed?sort=newest').as('feed');
+    cy.wait('@feed');
+    cy.getByTestId('veil').should('be.visible');
+  });
+});
