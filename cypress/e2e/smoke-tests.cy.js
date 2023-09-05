76c6a942dd6b7caeb5188ae3bdc5a8175c2d29bd
Toni Cabrera <acabreragnz@gmail.com>
2023-09-05 00:22:50 -0300

    feat: add smoke tests

---
+++ cypress/e2e/smoke-tests.cy.js
@@ -0,0 +1,26 @@
+describe('Smoke Tests', () => {
+  const pages = [
+    { url: '/', element: 'Acapedia is where the brightest minds of the world come together' },
+    { url: '/premium-courses', element: 'Boost your career, knowledge and learning with our exclusive content' },
+    { url: '/special-requirements', element: 'Complete your special requirements with our exclusive content' },
+    { url: '/login', element: 'Continue earning CME and MOC credits' },
+    { url: '/register', element: 'Join our brilliant medical community' },
+    { url: '/terms', element: 'Terms of Use' },
+    { url: '/privacy', element: 'Privacy Policy' },
+    { url: '/article/firefighers-pfas-donations-trial', element: 'Get CME' },
+    {
+      url: '/premium-courses/the-original-advanced-training-program-on-the-safe-of-use-of-fluoroscopy/abstract',
+      element: 'You need to have a paid account to access premium courses',
+    },
+  ];
+
+  pages.forEach((page) => {
+    it(`should load ${page.url} correctly`, () => {
+      cy.visit(page.url);
+
+      // Check if the main identifying element of the page is present
+      // This ensures the page has loaded and is displaying its main content
+      cy.contains(page.element).should('be.visible');
+    });
+  });
+});
