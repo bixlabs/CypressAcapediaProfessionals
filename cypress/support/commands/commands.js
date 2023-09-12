48cfd453227f735c63ae33eeb99933e59652a955
Toni Cabrera <acabreragnz@gmail.com>
2023-09-12 13:51:15 -0300

    test: add slowdown utility

--- cypress/support/commands/commands.js
+++ cypress/support/commands/commands.js
@@ -1,25 +1,29 @@
-Cypress.Commands.add('iframe', {prevSubject:'element'}, ($iframe, selector) => {
-    Cypress.log({
-        name:'iframe',
-        consoloreProps() {
-            return{
-                iframe:$iframe,
-            }
-        }
-    })
-    return new Cypress.Promise(resolve => {
-        resolve($iframe.contents().find(selector))
-    })
-})
+// In case you want to slow down a particular test, you can use the following snippet at the top:
+// import { slowCypressDown } from 'cypress-slow-down';
+// slowCypressDown();
+
+Cypress.Commands.add('iframe', { prevSubject: 'element' }, ($iframe, selector) => {
+  Cypress.log({
+    name: 'iframe',
+    consoloreProps() {
+      return {
+        iframe: $iframe,
+      };
+    },
+  });
+  return new Cypress.Promise((resolve) => {
+    resolve($iframe.contents().find(selector));
+  });
+});
 
 Cypress.Commands.add('getByTestId', (testId, ...args) => {
-    return cy.get(`[data-test-id=${testId}]`, {timeout: 15000, ...args})
-})
+  return cy.get(`[data-test-id=${testId}]`, { timeout: 15000, ...args });
+});
 
 Cypress.Commands.add('cleanUpDB', () => {
-    cy.request('POST', '/api/testing/db/clean-up', { timeout: 15000 })
-})
-  
-  Cypress.Commands.add('seedDB', () => {
-    cy.request('POST', '/api/testing/db/seed', { timeout: 15000 })
-})
+  cy.request('POST', '/api/testing/db/clean-up', { timeout: 15000 });
+});
+
+Cypress.Commands.add('seedDB', () => {
+  cy.request('POST', '/api/testing/db/seed', { timeout: 15000 });
+});
