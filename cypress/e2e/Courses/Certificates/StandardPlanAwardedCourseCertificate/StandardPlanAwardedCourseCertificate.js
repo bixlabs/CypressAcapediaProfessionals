5ac431a055247922746b767a29e2fdb457e25a70
Marco Caceres <marco@MacBook-Air-de-Marco.local>
2024-04-08 15:01:22 -0500

    feat: add standard awarded certificates remaining tests

--- cypress/e2e/Courses/Certificates/StandardPlanAwardedCourseCertificate/StandardPlanAwardedCourseCertificate.js
+++ cypress/e2e/Courses/Certificates/StandardPlanAwardedCourseCertificate/StandardPlanAwardedCourseCertificate.js
@@ -29,3 +29,52 @@ Then('the user should see the call to action "Review course" for awarded courses
     });
   });
 });
+
+// START Scenario: Standard plan users can successfully go to "Review course"
+Given('an iOS mobile device is not used', () => {
+  this.userAgent = undefined;
+});
+
+When('the "Completed" tab is selected', () => {
+  cy.intercept('GET', '/api/feed/premium-courses?enrollmentStatus=completed').as('completedCourses');
+  cy.contains('Completed').click();
+  cy.getByTestId('Completed').should('have.class', 'v-tab--active');
+});
+
+When('the call to action "Review course" is displayed to the user', () => {
+  cy.wait('@completedCourses').then((interception) => {
+    interception.response.body.data.forEach((course, index) => {
+      if (course.isAwarded) {
+        cy.getByTestId('status-completed').eq(index).contains('Review course').should('exist');
+      }
+    });
+  });
+});
+
+When('the user click to "Review course"', () => {
+  cy.contains('Review course').click();
+});
+
+Then('the user should be taken to "Review course"', () => {
+  // due that we are targeting specific users it doesn't matter we hardcoded the URL
+  cy.url().should('include', '/premium-courses/63/overview');
+});
+// END Scenario
+
+// START Scenario: Standard plan users can successfully download certificate
+Given('a standard plan users in the review course page', () => {
+  cy.intercept('GET', '/api/feed/premium-courses?enrollmentStatus=completed').as('completedCourses');
+  cy.contains('Completed').click();
+  cy.contains('Review course').click();
+  cy.url().should('include', '/premium-courses/63/overview');
+});
+Given('the call to action "Download certificate" is displayed to the user', () => {
+  cy.contains('Download certificate').should('exist');
+});
+When('the user requests to "Download certificate"', () => {
+  cy.contains('Download certificate').click();
+});
+Then('the certificate should be downloaded successfully', () => {
+  cy.readFile('./cypress/downloads/Testing_The_greatest_activity_of_all_the_time_2024.pdf').should('exist');
+});
+// END Scenario
