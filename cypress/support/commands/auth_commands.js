8feee134ee0573db47212919cb7d9cb024d2c7fd
Toni Cabrera <acabreragnz@gmail.com>
2024-10-28 23:20:04 -0300

    fix: avoid signup extra step because of lack of memory now in command

--- cypress/support/commands/auth_commands.js
+++ cypress/support/commands/auth_commands.js
@@ -27,18 +27,8 @@ Cypress.Commands.add(
     cy.getByTestId('firstName').type(firstName);
     cy.getByTestId('lastName').type(lastName);
     cy.get('[name=degree]').parent().click();
-    cy.contains('M.D').click();
+    cy.contains('Other').click();
     cy.contains('Continue').click();
-
-    // TODO: we need a test-id here as we cannot get it by text value
-    cy.get('#medical-board').parent().click();
-    cy.contains('American Board of Anesthesia').click();
-    cy.get('#boardId').type('123456');
-    cy.getByTestId('boardDateOfBirdInput').click();
-    cy.selectDate({ year: '1991', month: 'Oct', day: '10' });
-
-    // TODO: we need a test-id here as we cannot get it by text value
-    cy.contains('Continue').click({ force: true });
 
     cy.wait('@register').its('response.statusCode').should('equal', 200);
 
