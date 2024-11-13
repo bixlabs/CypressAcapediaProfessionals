247157e8150dd4b1fce711875c38f906eb7d4c67
Toni Cabrera <acabreragnz@gmail.com>
2024-11-13 19:05:08 -0300

    chore: increase wait time before taking visual snapshot in login test

--- cypress/e2e/Auth/LoginVisual.cy.js
+++ cypress/e2e/Auth/LoginVisual.cy.js
@@ -2,6 +2,7 @@ describe('Integration test with visual testing', function () {
   it('Loads the homepage', function () {
     // Load the page or perform any other interactions with the app.
     cy.visit('/login');
+    cy.wait(5000);
     // Take a snapshot for visual diffing
     cy.percySnapshot('login-desktop');
   });
