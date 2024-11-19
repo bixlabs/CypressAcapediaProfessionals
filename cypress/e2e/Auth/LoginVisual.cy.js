f48201ef1213f709c6cb7ebe70a8db8cbb8a9173
Toni Cabrera <acabreragnz@gmail.com>
2024-11-19 15:10:04 -0300

    feat: update visual testing for login page with viewport settings and snapshot widths

--- cypress/e2e/Auth/LoginVisual.cy.js
+++ cypress/e2e/Auth/LoginVisual.cy.js
@@ -2,8 +2,11 @@ describe('Integration test with visual testing', function () {
   it('Loads the homepage', function () {
     // Load the page or perform any other interactions with the app.
     cy.visit('/login');
+    cy.viewport('samsung-s10');
     cy.wait(5000);
     // Take a snapshot for visual diffing
-    cy.percySnapshot('login-desktop');
+    cy.percySnapshot('login-desktop', {
+      widths: [360],
+    });
   });
 });
