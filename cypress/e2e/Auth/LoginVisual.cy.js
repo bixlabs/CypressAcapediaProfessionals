62c50468c8f22ae5b9516b0bd100087b94cf9b20
Toni Cabrera <acabreragnz@gmail.com>
2024-11-20 12:48:13 -0300

    feat: update visual test for checkout referrals with API interception and snapshot adjustments

--- cypress/e2e/Auth/LoginVisual.cy.js
+++ cypress/e2e/Auth/LoginVisual.cy.js
@@ -1,12 +1,28 @@
 describe('Integration test with visual testing', function () {
-  it('Loads the homepage', function () {
+  it('Loads the checkout new referrals', function () {
+    cy.fixture('common/credentials').then((credentials) => {
+      cy.loginAccount(credentials.incompleteProfile);
+    });
+
+    cy.intercept('GET', 'api/subscription/change/preview?item=*', (req) => {
+      req.continue((res) => {
+        res.body.couponDiscount = 0;
+        res.body.paidReferralDiscount = 900;
+        res.body.paidReferralDiscountRemainingAfterPurchase = 23;
+        res.body.planOriginalBasePrice = 998;
+        res.body.totalToPaid = 0;
+        res.body.upgradeDiscount = 204;
+      });
+    });
+
     // Load the page or perform any other interactions with the app.
-    cy.visit('/login');
     cy.viewport('samsung-s10');
+    cy.visit('/checkout');
     cy.wait(5000);
     // Take a snapshot for visual diffing
-    cy.percySnapshot('login-desktop', {
+    cy.percySnapshot('payment-details-referrals', {
       widths: [360],
+      scope: '.payment-details',
     });
   });
 });
